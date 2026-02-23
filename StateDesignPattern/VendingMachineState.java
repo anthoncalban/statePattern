77a3884654d0578b3a9f8e5750e2d90d2b0665c5
Anthon Van Calban <calbananthon@gmail.com>
2026-02-23 11:04:49 +0800

    Delete StateDesignPattern directory

--- StateDesignPattern/VendingMachineState.java
+++
@@ -1,6 +0,0 @@
-public interface VendingMachineState {
-    void selectItem(VendingMachine machine, String item);
-    void insertCoin(VendingMachine machine, int amount);
-    void dispenseItem(VendingMachine machine);
-    void setOutOfOrder(VendingMachine machine);
-}
